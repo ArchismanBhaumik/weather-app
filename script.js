c753e77f44aff84a05e0bd63a494b1e40a6430c6
ArchismanBhaumik <archismanbhaumik786@gmail.com>
2024-07-02 00:14:08 +0530

    Final Push for weather app

---
+++ script.js
@@ -0,0 +1,20 @@
+const apiKey = "3fe0e4664838824576f22a7448e026a5";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+
+const searchBox = document.querySelector('.search input');
+const btn = document.querySelector('.search button');
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    document.querySelector(".city").textContent = data.name;
+    document.querySelector(".temp").textContent = Math.round(data.main.temp) + " °C";
+    document.querySelector(".humidity").textContent = data.main.humidity + " %";
+    document.querySelector(".wind").textContent = data.wind.speed + " km/hr";
+}
+
+btn.addEventListener("click",() =>{
+    checkWeather(searchBox.value)
+});
+
