c784dabe61e3d91dcab9eed8414f79b67ce6df20
ArchismanBhaumik <archismanbhaumik786@gmail.com>
2024-07-13 14:28:58 +0530

    validation

--- script.js
+++ script.js
@@ -5,13 +5,16 @@ const searchBox = document.querySelector('.search input');
 const btn = document.querySelector('.search button');
 
 async function checkWeather(city) {
-    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    var data = await response.json();
-
-    document.querySelector(".city").textContent = data.name;
-    document.querySelector(".temp").textContent = Math.round(data.main.temp) + " °C";
-    document.querySelector(".humidity").textContent = data.main.humidity + " %";
-    document.querySelector(".wind").textContent = data.wind.speed + " km/hr";
+    if(city){
+        const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+        var data = await response.json();
+    
+        document.querySelector(".city").textContent = data.name;
+        document.querySelector(".temp").textContent = Math.round(data.main.temp) + " °C";
+        document.querySelector(".humidity").textContent = data.main.humidity + " %";
+        document.querySelector(".wind").textContent = data.wind.speed + " km/hr";
+    }
+    
 }
 
 btn.addEventListener("click",() =>{
